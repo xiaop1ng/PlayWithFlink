1925794ce3ac764e6c5f41d242c11e2412aafe1e
xiaop1ng <jianchaoping@gmail.com>
2023-07-04 11:36:02 +0800

    kafka source -> flink -> db sink

--- src/main/java/org/play/datastream/DataStreamKafkaDemo.java
+++ src/main/java/org/play/datastream/DataStreamKafkaDemo.java
@@ -1,17 +1,25 @@
 package org.play.datastream;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 
 public class DataStreamKafkaDemo {
 
     public static void main(String[] args) throws Exception{
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        DataStreamSource<String> source = env.socketTextStream("localhost", 9000);
-
+        KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
+                        .setBootstrapServers("localhost:9092")
+                        .setTopics("topic-test")
+                        .setGroupId("my-group")
+                        .setValueOnlyDeserializer(new SimpleStringSchema())
+                        .build();
+        DataStreamSource<String> source = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka Source");
         source.print();
-
         env.execute("DataStreamKafkaDemo");
     }
 
